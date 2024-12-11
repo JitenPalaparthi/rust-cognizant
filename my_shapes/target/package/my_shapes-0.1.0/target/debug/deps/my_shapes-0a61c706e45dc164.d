990f74515c710539dd389ae573fdcde82aeb6c63
Jiten <jpalaparthi@gmail.com>
2024-12-11 21:24:13 +0530

    add all files

---
+++ my_shapes/target/package/my_shapes-0.1.0/target/debug/deps/my_shapes-0a61c706e45dc164.d
@@ -0,0 +1,11 @@
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/package/my_shapes-0.1.0/target/debug/deps/libmy_shapes-0a61c706e45dc164.rmeta: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs
+
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/package/my_shapes-0.1.0/target/debug/deps/libmy_shapes-0a61c706e45dc164.rlib: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs
+
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/package/my_shapes-0.1.0/target/debug/deps/my_shapes-0a61c706e45dc164.d: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs
+
+src/lib.rs:
+src/shapes.rs:
+src/shapes/cuboid.rs:
+src/shapes/rect.rs:
+src/shapes/square.rs:
---
+++ my_shapes/target/package/my_shapes-0.1.0/target/debug/deps/my_shapes-2cc882b32f136a38.d
@@ -0,0 +1,13 @@
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/debug/deps/libmy_shapes-2cc882b32f136a38.rmeta: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs Cargo.toml
+
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/debug/deps/my_shapes-2cc882b32f136a38.d: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs Cargo.toml
+
+src/lib.rs:
+src/shapes.rs:
+src/shapes/cuboid.rs:
+src/shapes/rect.rs:
+src/shapes/square.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
