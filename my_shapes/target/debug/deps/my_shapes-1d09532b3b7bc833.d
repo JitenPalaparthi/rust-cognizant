6c759421d83c4ca3cdbad3834a39e1db8bc0ea78
Jiten <jpalaparthi@gmail.com>
2024-12-11 21:18:58 +0530

    add to git

---
+++ my_shapes/target/debug/deps/my_shapes-1d09532b3b7bc833.d
@@ -0,0 +1,9 @@
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/debug/deps/libmy_shapes-1d09532b3b7bc833.rmeta: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs
+
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/debug/deps/my_shapes-1d09532b3b7bc833.d: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs
+
+src/lib.rs:
+src/shapes.rs:
+src/shapes/cuboid.rs:
+src/shapes/rect.rs:
+src/shapes/square.rs:
---
+++ my_shapes/target/debug/deps/my_shapes-56fa589220af0c0d.d
@@ -0,0 +1,9 @@
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/debug/deps/my_shapes-56fa589220af0c0d: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs
+
+/Users/jiten/workspace/training/rust-cognizant/my_shapes/target/debug/deps/my_shapes-56fa589220af0c0d.d: src/lib.rs src/shapes.rs src/shapes/cuboid.rs src/shapes/rect.rs src/shapes/square.rs
+
+src/lib.rs:
+src/shapes.rs:
+src/shapes/cuboid.rs:
+src/shapes/rect.rs:
+src/shapes/square.rs:
